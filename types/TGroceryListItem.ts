035aab92ef3bfbf1e0dc7cf5fffb5bc90c7d10b4
tom jielis van wijgerden <tomjielisvanwijgerden@MacBook-Pro-van-tom.local>
2025-08-17 13:18:24 +0200

    added types

---
+++ types/TGroceryListItem.ts
@@ -0,0 +1,10 @@
+// ~/types/user.ts
+export type TGroceryList = {
+    id: number;
+    name: string;
+    created_by: number;
+    created_at: string;
+    updated_at: string;
+    grocery_list_items_count: number;
+    grocery_list_items_checked_count: number;
+}
