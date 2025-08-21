75948eea51231e56d81df65b5f11c43bc24e11f5
tom jielis van wijgerden <tomjielisvanwijgerden@MacBook-Pro-van-tom.local>
2025-08-21 22:33:47 +0200

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,7 +8,7 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
     api: {
-      url: process.env.API_URL || 'http://0.0.0.0:8000',
+      url: process.env.API_URL || '0.0.0.0:8000',
       basePath: process.env.API_BASE_PATH || '/api',
     },
   },
