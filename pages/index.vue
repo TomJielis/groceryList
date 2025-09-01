787663bbcb200448991c2574c3bf36ee53a779cc
tom jielis van wijgerden <tomjielisvanwijgerden@MacBook-Pro-van-tom.local>
2025-09-01 20:05:01 +0200

    fix

--- pages/index.vue
+++ pages/index.vue
@@ -44,8 +44,8 @@ const favoriteListOpenItems = favoriteList.grocery_list_items_count - favoriteLi
       </h1>
       <div class="grid grid-cols-1 md:grid-cols-2 gap-6 mb-8">
         <dashboardBlock :title="'total items to buy'" :count="totalUncheckedItems" @click="$router.push('/list/lists')" class="cursor-pointer"/>
-        <dashboardBlock v-if="authStore.user.favorite_list_id > 0" :title="favoriteListOpenItems + ' items to buy'" :count="' ⭐' + favoriteList.name"   @click="$router.push(favoriteListUrl)" class="cursor-pointer"/>
-        <dashboardBlock v-if="authStore.user.favorite_list_id === null" :title="'Lists'" @click="$router.push('/list/lists')" :count="parseInt(countLists)" class="cursor-pointer" />
+        <dashboardBlock v-if="authStore.user?.favorite_list_id > 0" :title="favoriteListOpenItems + ' items to buy'" :count="' ⭐' + favoriteList.name"   @click="$router.push(favoriteListUrl)" class="cursor-pointer"/>
+        <dashboardBlock v-if="authStore.user?.favorite_list_id === null" :title="'Lists'" @click="$router.push('/list/lists')" :count="parseInt(countLists)" class="cursor-pointer" />
       </div>
       <topItems />
     </div>
