45c43540b2980c61712d607e0163e79ca703208d
tom jielis van wijgerden <tomjielisvanwijgerden@MacBook-Pro-van-tom.local>
2025-09-01 20:52:04 +0200

    new background

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="page-wrapper">
+    <NuxtPage />
+  </div>
+</template>
+
+<style scoped>
+.page-wrapper {
+  background: transparent; /* ✅ allow body background to show through */
+  min-height: 100vh;
+}
+</style>
