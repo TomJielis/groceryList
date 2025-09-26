03fe9b96d4d7a50f510e784a30c5b9ea8be43b44
tom jielis van wijgerden <tomjielisvanwijgerden@MacBook-Pro-van-tom.local>
2025-09-26 17:49:58 +0200

    fix

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -16,7 +16,7 @@ function logoutUser() {
     <div class="container mx-auto flex justify-between items-center">
       <h1 class="text-xl font-bold"><nuxtLink to="/">Grocery List</nuxtLink></h1>
       <ul class="flex space-x-4">
-        <nuxtLink v-if="authStore.user" to="/list/lists" replace class="hover:underline">Lists</nuxtLink>
+        <nuxtLink v-if="authStore.user" to="/cards/" replace class="hover:underline">Cards</nuxtLink>
         <nuxtLink v-if="!authStore.user" to="/auth/login" replace class="hover:underline">Login</nuxtLink>
         <nuxtLink v-if="!authStore.user" to="/auth/register" replace class="hover:underline">Register</nuxtLink>
         <nuxtLink v-if="authStore.user" @click="logoutUser" class="hover:underline">Logout</nuxtLink>
