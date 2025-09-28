9e4df88d68d13484592121a0685baf74ead195de
tom jielis van wijgerden <tomjielisvanwijgerden@MacBook-Pro-van-tom.local>
2025-09-28 22:23:43 +0200

    give some more space to the application

--- components/list/AddItemListForm.vue
+++ components/list/AddItemListForm.vue
@@ -48,7 +48,7 @@ const filteredSuggestions = computed(() => {
 fetchItems(listId)
 </script>
 <template>
-  <div class="flex-auto overflow-y-auto p-4 pb-10 md:pb-4 h-[80vh]">
+  <div class="flex-auto overflow-y-auto p-4 pb-20 md:pb-4 h-[80vh]">
     <div class="items-center flex-col">
       <input
           type="text"
