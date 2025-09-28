7f6f06a3165279279c17c8672be63d3e7111d957
tom jielis van wijgerden <tomjielisvanwijgerden@MacBook-Pro-van-tom.local>
2025-09-28 21:54:04 +0200

    fix

--- components/list/AddItemListForm.vue
+++ components/list/AddItemListForm.vue
@@ -2,9 +2,10 @@
 import {ref, computed} from 'vue';
 import {useRoute} from 'vue-router';
 import {useGroceryList} from '~/composables/useGroceryList';
-import { useSuggestionStore } from '~/stores/suggestions'
+import {useSuggestionStore} from '~/stores/suggestions'
 
 import suggestionsData from '~/data/suggestions.json';
+
 const {
   addItem,
   items,
@@ -41,62 +42,66 @@ const filteredSuggestions = computed(() => {
       item.name.toLowerCase().includes(newItem.value.toLowerCase())
   );
   const isDuplicate = suggestions.some(item => item.name.toLowerCase() === newItem.value.toLowerCase());
-  return newItem.value && !isDuplicate ? [{ name: newItem.value, checked:false }, ...suggestions] : suggestions;
+  return newItem.value && !isDuplicate ? [{name: newItem.value, checked: false}, ...suggestions] : suggestions;
 });
 
 fetchItems(listId)
 </script>
 <template>
-  <div class="flex-auto overflow-y-auto p-4 pb-32 md:pb-4 h-[80vh]">
-      <div class="items-center flex-col">
-        <input
-            type="text"
-            v-model="newItem"
-            placeholder="Nieuwe item invoeren"
-            class="w-full mb-3 px-4 py-3 rounded-xl border border-gray-300 focus:ring-2 focus:ring-blue-400 focus:border-blue-400 text-base sm:text-lg md:text-xl"
-        />
-        <ul class="space-y-3">
-          <li
-              v-for="item in filteredSuggestions"
-              :key="item.name"
-              class="flex items-center justify-between bg-white rounded-xl shadow-sm p-3 sm:p-4 md:p-5"
-          >
-            <div class="flex items-center justify-between w-full">
-              <div class="flex items-center space-x-2">
-                <button @click="addItemToList(item.name)" :class="items.some(listItem => listItem.name.toLowerCase() === item.name.toLowerCase()) ? 'text-green-500' : 'text-black'">
-                  {{ items.filter(item => !item.checked).some(listItem => listItem.name.toLowerCase() === item.name.toLowerCase()) ? '✔️' : '➕' }}
-                </button>
-                <span class="text-sm sm:text-base md:text-lg">{{ item.name }}</span>
-              </div>
-              <div class="flex items-center space-x-2" v-if="items.filter(item => !item.checked).some(listItem => listItem.name.toLowerCase() === item.name.toLowerCase())">
-                <button
-                    class="w-8 h-8 sm:w-9 sm:h-9 flex items-center justify-center bg-gray-200 rounded-full text-base sm:text-lg font-bold hover:bg-gray-300"
-                    @click="decreaseItems(items.find(listItem => listItem.name.toLowerCase() === item.name.toLowerCase()))"
-                >−
-                </button>
+  <div class="flex-auto overflow-y-auto p-4 pb-10 md:pb-4 h-[80vh]">
+    <div class="items-center flex-col">
+      <input
+          type="text"
+          v-model="newItem"
+          placeholder="Nieuwe item invoeren"
+          class="w-full mb-3 px-4 py-3 rounded-xl border border-gray-300 focus:ring-2 focus:ring-blue-400 focus:border-blue-400 text-base sm:text-lg md:text-xl"
+      />
+      <ul class="space-y-3">
+        <li
+            v-for="item in filteredSuggestions"
+            :key="item.name"
+            class="flex items-center justify-between bg-white rounded-xl shadow-sm p-3 sm:p-4 md:p-5"
+        >
+          <div class="flex items-center justify-between w-full">
+            <div class="flex items-center space-x-2">
+              <button @click="addItemToList(item.name)"
+                      :class="items.some(listItem => listItem.name.toLowerCase() === item.name.toLowerCase()) ? 'text-green-500' : 'text-black'">
+                {{
+                  items.filter(item => !item.checked).some(listItem => listItem.name.toLowerCase() === item.name.toLowerCase()) ? '✔️' : '➕'
+                }}
+              </button>
+              <span class="text-sm sm:text-base md:text-lg">{{ item.name }}</span>
+            </div>
+            <div class="flex items-center space-x-2"
+                 v-if="items.filter(item => !item.checked).some(listItem => listItem.name.toLowerCase() === item.name.toLowerCase())">
+              <button
+                  class="w-8 h-8 sm:w-9 sm:h-9 flex items-center justify-center bg-gray-200 rounded-full text-base sm:text-lg font-bold hover:bg-gray-300"
+                  @click="decreaseItems(items.find(listItem => listItem.name.toLowerCase() === item.name.toLowerCase()))"
+              >−
+              </button>
 
-                <span class="text-sm sm:text-base font-semibold min-w-[20px] sm:min-w-[24px] text-center">
+              <span class="text-sm sm:text-base font-semibold min-w-[20px] sm:min-w-[24px] text-center">
                    {{ items.find(listItem => listItem.name.toLowerCase() === item.name.toLowerCase())?.quantity ?? 1 }}
                 </span>
 
-                <button
-                    v-if="items.filter(item => !item.checked).some(listItem => listItem.name.toLowerCase() === item.name.toLowerCase())"
-                    class="w-8 h-8 sm:w-9 sm:h-9 flex items-center justify-center bg-gray-200 rounded-full text-base sm:text-lg font-bold hover:bg-gray-300"
-                    @click="increaseItems(items.find(listItem => listItem.name.toLowerCase() === item.name.toLowerCase()))"
-                >+
-                </button>
-              </div>
+              <button
+                  v-if="items.filter(item => !item.checked).some(listItem => listItem.name.toLowerCase() === item.name.toLowerCase())"
+                  class="w-8 h-8 sm:w-9 sm:h-9 flex items-center justify-center bg-gray-200 rounded-full text-base sm:text-lg font-bold hover:bg-gray-300"
+                  @click="increaseItems(items.find(listItem => listItem.name.toLowerCase() === item.name.toLowerCase()))"
+              >+
+              </button>
             </div>
-          </li>
-        </ul>
-      </div>
+          </div>
+        </li>
+      </ul>
     </div>
+  </div>
   <div class="fixed left-0 w-full mt-6 space-y-3 p-4 bottom-20 md:bottom-3 z-50">
-      <button
-          class="w-full py-3 rounded-xl bg-gray-200 text-gray-700 font-medium text-base hover:bg-gray-300 transition"
-          @click="$emit('close')"
-      >
-        ← Terug
-      </button>
-    </div>
+    <button
+        class="w-full py-3 rounded-xl bg-gray-200 text-gray-700 font-medium text-base hover:bg-gray-300 transition"
+        @click="$emit('close')"
+    >
+      ← Terug
+    </button>
+  </div>
 </template>
