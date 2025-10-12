3d87b542ce3fdc2ae0334dc5315e99e20b4a2ff3
tom jielis van wijgerden <tomjielisvanwijgerden@MacBook-Pro-van-tom.local>
2025-10-12 18:30:15 +0200

    fix

--- server/api/groceryListItem/index.ts
+++ server/api/groceryListItem/index.ts
@@ -14,7 +14,5 @@ export default defineEventHandler(async (event) => {
             body: { listId:listId },
         }, token);
 
-    console.log(response);
-
-    return response;
+    return response as unknown as TGroceryListItem[];
 });
