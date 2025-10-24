b7f971f72195731a5fc3a16760b5d867a2d68182
tom jielis van wijgerden <tomjielisvanwijgerden@MacBook-Pro-van-tom.local>
2025-10-24 17:52:59 +0200

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,40 +1,32 @@
-import {defineNuxtConfig} from 'nuxt/config'
+import { defineNuxtConfig } from 'nuxt/config'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 export default defineNuxtConfig({
-    app: {
-        head: {
-            meta: [
-                // Algemene theme kleur
-                {name: 'theme-color', content: '#0b1220'},
-
-                // iOS specifieke instellingen
-                {name: 'apple-mobile-web-app-capable', content: 'yes'},
-                // Probeer "default" in plaats van "black-translucent"
-                {name: 'apple-mobile-web-app-status-bar-style', content: 'default'},
-                {name: 'apple-mobile-web-app-title', content: 'GroceryList'},
-            ],
-            link: [
-                {rel: 'manifest', href: '/manifest.json'},
-                {rel: 'apple-touch-icon', href: '/icons/app-icon-3.png'}
-            ]
-        }
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'manifest',
+          href: '/manifest.json?v=1.0.5'
+        } as any
+      ],
+    }
+  },
+  modules: ['@pinia/nuxt'],
+  css: ['@/assets/css/tailwind.css', '@/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
     },
-    modules: ['@pinia/nuxt'],
-    css: ['@/assets/css/tailwind.css', '@/assets/css/main.css'],
-    postcss: {
-        plugins: {
-            tailwindcss: {},
-            autoprefixer: {},
-        },
+  },
+  runtimeConfig: {
+    api: {
+      url: process.env.API_URL || '0.0.0.0:8000',
+      basePath: process.env.API_BASE_PATH || '/api',
     },
-    runtimeConfig: {
-        api: {
-            url: process.env.API_URL || '0.0.0.0:8000',
-            basePath: process.env.API_BASE_PATH || '/api',
-        },
-    },
-    nitro: {
-        serverDir: 'server/nuxt-api',
-    },
+  },
+  nitro: {
+    serverDir: 'server/nuxt-api',
+  },
 })
