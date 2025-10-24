9726a80b1a9cfad461ce0c2c23a2ea4730c46f6d
tom jielis van wijgerden <tomjielisvanwijgerden@MacBook-Pro-van-tom.local>
2025-10-24 18:01:31 +0200

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,32 +1,46 @@
-import { defineNuxtConfig } from 'nuxt/config'
+import {defineNuxtConfig} from 'nuxt/config'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 export default defineNuxtConfig({
-  app: {
-    head: {
-      link: [
-        {
-          rel: 'manifest',
-          href: '/manifest.json?v=1.0.5'
-        } as any
-      ],
-    }
-  },
-  modules: ['@pinia/nuxt'],
-  css: ['@/assets/css/tailwind.css', '@/assets/css/main.css'],
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
+    app: {
+        head: {
+            title: 'GroceryList – Maak en deel je boodschappenlijsten makkelijk',
+            meta: [
+                {
+                    name: 'description',
+                    content: 'GroceryList is de makkelijke app om boodschappenlijsten te maken, delen en bijhouden.'
+                },
+                {property: 'og:title', content: 'GroceryList'},
+                {property: 'og:description', content: 'Maak en deel boodschappenlijsten met vrienden of familie.'},
+                {property: 'og:type', content: 'website'},
+                {property: 'og:url', content: 'https://tomjielis.com'}
+            ],
+            link: [
+                {
+                    rel: 'manifest',
+                    href: '/manifest.json?v=1.0.5'
+                } as any
+            ],
+        }
     },
-  },
-  runtimeConfig: {
-    api: {
-      url: process.env.API_URL || '0.0.0.0:8000',
-      basePath: process.env.API_BASE_PATH || '/api',
+    modules: ['@pinia/nuxt'],
+    sitemap: {
+        hostname: 'https://tomjielis.com',
     },
-  },
-  nitro: {
-    serverDir: 'server/nuxt-api',
-  },
+    css: ['@/assets/css/tailwind.css', '@/assets/css/main.css'],
+    postcss: {
+        plugins: {
+            tailwindcss: {},
+            autoprefixer: {},
+        },
+    },
+    runtimeConfig: {
+        api: {
+            url: process.env.API_URL || '0.0.0.0:8000',
+            basePath: process.env.API_BASE_PATH || '/api',
+        },
+    },
+    nitro: {
+        serverDir: 'server/nuxt-api',
+    },
 })
