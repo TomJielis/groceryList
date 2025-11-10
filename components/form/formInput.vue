c2c5c10ea3242db340f2c82aa39b23b587e9dab0
Tom Jielis van Wijgerden <tomvanwijgerden@gmail.com>
2025-11-10 22:14:02 +0100

    if disabled. make sure it gets a gray glow over it

--- components/form/formInput.vue
+++ components/form/formInput.vue
@@ -22,6 +22,10 @@ const emit = defineEmits<{
       @input="emit('update:modelValue', $event.target.value)"
       :placeholder="placeholder"
       :disabled="disabled"
-      class="w-full mb-3 px-4 py-3 rounded-xl border border-gray-300 focus:ring-2  border-border-light dark:border-border-dark focus:ring-blue-400 focus:border-blue-400 text-base placeholder-slate-400 dark:placeholder-slate-500 bg-white/80 dark:bg-slate-900/80"
+      :class="[
+        'w-full mb-3 px-4 py-3 rounded-xl border border-gray-300 focus:ring-2 border-border-light dark:border-border-dark focus:ring-blue-400 focus:border-blue-400 text-base placeholder-slate-400 dark:placeholder-slate-500',
+        'bg-white/90 dark:bg-slate-900/80',
+        disabled ? 'bg-gray-00 dark:bg-gray-800 text-gray-200' : ''
+      ]"
   />
 </template>
