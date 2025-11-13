bcdf2ba9c37be0052d16edfa734acce593c09026
Tom Jielis van Wijgerden <tomvanwijgerden@gmail.com>
2025-11-13 21:02:00 +0100

    wip for approving new terms

---
+++ server/api/user/approveTerms.ts
@@ -0,0 +1,17 @@
+import {readBody} from 'h3'
+import { getCookie } from 'h3'
+import {apiClient} from "~/server/api/utils/apiClient";
+
+export default defineEventHandler(async (event) => {
+    const token = getCookie(event, 'token')
+
+    try {
+        const response = await apiClient('/approve-terms',
+            {
+                method: 'POST',
+            }, token);
+        return response;
+    } catch (error) {
+        throw new Error(`Failed to fetch data: ${error}`);
+    }
+});
