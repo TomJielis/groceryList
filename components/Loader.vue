f13127940f2cfed91939ea608fe5ccb223a8a270
Tom van Wijgerden <tom.vanwijgerden@visma.com>
2025-11-20 20:02:45 +0100

    add loader to the index.vue

---
+++ components/Loader.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="flex justify-center items-center p-10">
+    <div class="w-12 h-12 border-4 border-t-transparent border-blue-500 rounded-full animate-spin"></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+.animate-spin {
+  animation: spin 1s linear infinite;
+}
+@keyframes spin {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+</style>
+
