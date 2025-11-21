c51e9486e190a7916e234ccd94855673160672b7
Tom Jielis van Wijgerden <tomvanwijgerden@gmail.com>
2025-11-21 11:10:46 +0100

    set loader in the middle of the screen

--- components/Loader.vue
+++ components/Loader.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex justify-center items-center p-10">
+  <div class="fixed inset-0 flex justify-center items-center pb-20">
     <div class="w-12 h-12 border-4 border-t-transparent border-blue-500 rounded-full animate-spin"></div>
   </div>
 </template>
