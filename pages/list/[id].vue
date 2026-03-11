1eb765598dfb3675f64188bd5809cfe7eb4ecded
Tom van Wijgerden <tom.vanwijgerden@visma.com>
2026-03-11 11:42:30 +0100

    fix translation

--- pages/list/[id].vue
+++ pages/list/[id].vue
@@ -171,7 +171,7 @@ const list = listStore.lists.find((list: any) => list.id == parseInt(listId));
                 <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" d="M12 4v16m8-8H4"/>
                 </svg>
-                <span>{{ i18n.t('items.addFirst') }}</span>
+                <span>{{ i18n.t('items.addNew') }}</span>
               </button>
             </div>
           </div>
